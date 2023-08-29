4acbb062b1b9698ba44273c697dd60769515807c
Ivan Voloshchuk <keranamv@gmail.com>
2023-08-29 21:10:05 +0200

    Initial Commit

---
+++ AnimeQL/AnimeQLApp.swift
@@ -0,0 +1,20 @@
+//
+//  AnimeQLApp.swift
+//  AnimeQL
+//
+//  Created by Ivan Voloshchuk on 29/08/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AnimeQLApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Item.self)
+    }
+}
